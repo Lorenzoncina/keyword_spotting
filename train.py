1b3135d75ac27b97a4bccc499a4efed841d3bcde
concina <concina@ip-10-41-216-44.eu-central-1.compute.internal>
2024-09-04 08:08:29 +0000

    training script and requirement file for the env

--- train.py
+++ train.py
@@ -9,10 +9,11 @@ LEARNING_RATE = 0.0001
 EPOCHS = 40
 BATCH_SIZE = 32
 SAVED_MODEL_PATH = "model.h5"
+NUM_KEYWORDS = 10
 
 def load_dataset(data_path):
     with open(data_path, 'r') as fp:
-        data = json.load(data_path)
+        data = json.load(fp)
     #extract input and target
     X = np.array(data["MFCCs"])
     y = np.array(data["labels"])
@@ -27,13 +28,46 @@ def get_data_splits(data_path):
     
     #convert input from 2d to 3d array
     X_train = X_train[..., np.newaxis]
-    X_validation = X_train[..., np.newaxis]
+    X_validation = X_validation[..., np.newaxis]
     X_test = X_test[..., np.newaxis]
 
     return  X_train, X_validation, X_test,  y_train, y_validation, y_test
     
-def build_model()
-    pass
+def build_model(input_shape, learning_rate, error="sparse_categorical_crossentropy"):
+
+    #build network
+    model = keras.Sequential()
+    
+    #conv1
+    model.add(keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=input_shape, kernel_regularizer=keras.regularizers.l2(0.001)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.MaxPool2D((3,3), strides=(2,2), padding="same"))
+    
+    #conv2
+    model.add(keras.layers.Conv2D(32, (3,3), activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.MaxPool2D((3,3), strides=(2,2), padding="same"))
+    
+    #conv3
+    model.add(keras.layers.Conv2D(32, (2,2), activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.MaxPool2D((2,2), strides=(2,2), padding="same"))
+    
+    #flatten the output and feed it into a dense layer
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation="relu"))
+    model.add(keras.layers.Dropout(0.3))
+    
+    #softmax classfifier
+    model.add(keras.layers.Dense(NUM_KEYWORDS, activation="softmax")) 
+    
+    #compile the model
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(optimizer=optimizer, loss=error, metrics=["accuracy"])
+
+    #print model overview
+    model.summary()
+    return model
     
 def main():
     
@@ -53,3 +87,6 @@ def main():
     
     #save the model
     model.save(SAVED_MODEL_PATH)
+
+if __name__ == "__main__":
+    main()
